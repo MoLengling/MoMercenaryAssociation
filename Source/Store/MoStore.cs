40d0e33225fba2989149b542465324902721d7d3
MoLengling <1519716442@qq.com>
2022-08-31 14:01:17 +0800

    无意义的的store提交

---
+++ Source/Store/MoStore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaleWorlds.CampaignSystem.Inventory;
+using TaleWorlds.Core;
+
+namespace MoMercenaryAssociation.Source.Store
+{
+    class MoStore
+    {
+        ItemObject Item;
+        public MoStore() {
+            InventoryManager
+                
+                
+                }
+    }
+
+}
